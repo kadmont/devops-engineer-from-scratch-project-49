dc8145913edbcfb8a74aa44c86ed9b5b611b0bcd
kadmont <poshekhov@bk.ru>
2026-07-28 10:54:56 +0300

    add brain_games module with welcome message

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,7 @@
+def main():
+    print("Welcome to the Brain Games!")
+
+# При запуске модуля как программы сработает именно main().
+if __name__ == "__main__":
+# В main содержиться приветствие
+    main()
